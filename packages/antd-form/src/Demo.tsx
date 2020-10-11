042fb731d6cb9b6bdac7b72704e00a489e829624
Lin Chen <lc1990linux@gmail.com>
2020-10-11 20:34:12 +0800

    [Chen] add a demo

--- packages/antd-form/src/Demo.tsx
+++ packages/antd-form/src/Demo.tsx
@@ -47,54 +47,60 @@ const Demo = () => {
   console.log('Line 47', form.getFieldsValue());
 
   return (
-    <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
-      <Form.Item name="note" label="Note" rules={[{ required: true }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
-        <Select
-          placeholder="Select a option and change input text above"
-          onChange={onGenderChange}
-          allowClear
+    <Form.Provider
+      onFormFinish={(name, { values, forms }) => {
+        console.log('Line 10', name, values, forms);
+      }}
+    >
+      <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+        <Form.Item name="note" label="Note" rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
+          <Select
+            placeholder="Select a option and change input text above"
+            onChange={onGenderChange}
+            allowClear
+          >
+            <Option value="male">male</Option>
+            <Option value="female">female</Option>
+            <Option value="other">other</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          noStyle
+          shouldUpdate={(prevValues, currentValues) =>
+            prevValues.gender !== currentValues.gender
+          }
         >
-          <Option value="male">male</Option>
-          <Option value="female">female</Option>
-          <Option value="other">other</Option>
-        </Select>
-      </Form.Item>
-      <Form.Item
-        noStyle
-        shouldUpdate={(prevValues, currentValues) =>
-          prevValues.gender !== currentValues.gender
-        }
-      >
-        {({ getFieldValue }) => {
-          return getFieldValue('gender') === 'other' ? (
-            <Form.Item
-              name="customizeGender"
-              label="Customize Gender"
-              rules={[{ required: true }]}
-            >
-              <Input />
-            </Form.Item>
-          ) : null;
-        }}
-      </Form.Item>
-      <Form.Item name="myfield" label="my field" dependencies={['note']}>
-        <MyField data={form.getFieldsValue()} />
-      </Form.Item>
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-        <Button htmlType="button" onClick={onReset}>
-          Reset
-        </Button>
-        <Button type="link" htmlType="button" onClick={onFill}>
-          Fill form
-        </Button>
-      </Form.Item>
-    </Form>
+          {({ getFieldValue }) => {
+            return getFieldValue('gender') === 'other' ? (
+              <Form.Item
+                name="customizeGender"
+                label="Customize Gender"
+                rules={[{ required: true }]}
+              >
+                <Input />
+              </Form.Item>
+            ) : null;
+          }}
+        </Form.Item>
+        <Form.Item name="myfield" label="my field">
+          <MyField form={form} />
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+          <Button htmlType="button" onClick={onReset}>
+            Reset
+          </Button>
+          <Button type="link" htmlType="button" onClick={onFill}>
+            Fill form
+          </Button>
+        </Form.Item>
+      </Form>
+    </Form.Provider>
   );
 };
 
--- packages/antd-form/src/MyField.tsx
+++ packages/antd-form/src/MyField.tsx
@@ -2,8 +2,8 @@ import { Input } from 'antd';
 import React from 'react';
 
 const MyField = (props: any) => {
-  console.log('Line 4', props);
-  return <Input onChange={props.onChange}/>;
+  console.log('Line 4', props.form.getFieldsValue());
+  return <Input onChange={props.onChange} />;
 };
 
 export default MyField;
